4c44fa77d8cc2dcb00b4e9136f4200ac05c5d399
marduke182 <jrqb182@gmail.com>
2018-06-22 09:42:40 +0200

    feat: add is visible

--- packages/puppeteer-jest/src/utils.js
+++ packages/puppeteer-jest/src/utils.js
@@ -1,6 +1,7 @@
 const getElement = require('./utils/getElement');
 const hasElement = require('./utils/hasElement');
 const getText = require('./utils/getText');
+const isVisible = require('./utils/isVisible');
 const getHtml = require('./utils/getHtml');
 const getId = require('./utils/getId');
 const isChecked = require('./utils/isChecked');
@@ -13,6 +14,7 @@ module.exports = page => ({
   getElement: getElement.bind(null, page),
   hasElement: hasElement.bind(null, page),
   getText: getText.bind(null, page),
+  isVisible: isVisible.bind(null, page),
   getHtml: getHtml.bind(null, page),
   getId: getId.bind(null, page),
   isChecked: isChecked.bind(null, page),
